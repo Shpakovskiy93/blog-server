c8c9a881f3a277e6b34204ca48a68d5d63c79837
Shpakovskiy93 <mikhail.shpakovskiy.93@gmail.com>
2023-06-25 21:54:48 +0300

    blog server

---
+++ controllers/UserController.js
@@ -0,0 +1,115 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"; // password encryption 
+import { validationResult } from "express-validator";
+import UserModel from "../models/User.js";
+
+export const registr = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    // password encryption
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email, //
+      fullName: req.body.fullName, //  Create user
+      avatarUrl: req.body.avatarUrl, //
+      passwordHash: hash, //
+    });
+
+    const user = await doc.save(); // save in mongo DB
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "failed to register",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "user not found",
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: "wrong login or email",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "failed to authenticate",
+    });
+  }
+};
+
+export const getMe = async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "user not found",
+      });
+    }
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json(userData);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "No access",
+    });
+  }
+};
---
+++ validations.js
@@ -0,0 +1,20 @@
+import { body } from "express-validator";
+
+export const loginValidation = [
+    body('email', 'invalid mail').isEmail(),
+    body('password', 'password must be at least 5 characters').isLength({min: 5}),
+];
+
+export const registerValidation = [
+    body('email', 'invalid mail').isEmail(),
+    body('password', 'password must be at least 5 characters').isLength({min: 5}),
+    body('fullName', 'enter the correct name').isLength({min: 3}),
+    body('avatarUrl', 'wrong avatar link').optional().isURL(),
+];
+
+export const postCreateValidation = [
+    body('title', 'enter the title of the article').isLength({min: 3}).isString(),
+    body('text', 'enter the text of the article').isLength({min: 3}).isString(),
+    body('tags', 'invalid tag format').optional().isString(),
+    body('imageUrl', 'wrong image link').optional().isString(),
+];
