585162f519308a139dd044c4c121fae7a815d5ec
Shpakovskiy93 <mikhail.shpakovskiy.93@gmail.com>
2023-06-26 18:48:31 +0300

    add remove, update, get one post and get all post methods

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,91 @@
 import PostModel from '../models/Post.js';
 
+export const getAll = async (req, res) => {
+    try {
+        const posts = await PostModel.find().populate('user').exec();
+
+        res.json(posts);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'articles not found'
+        })
+    }
+};
+
+export const getOne = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        PostModel.findOneAndUpdate(
+            {
+                _id: postId,
+            }, 
+            {
+                $inc: {viewsCount: 1},
+            },
+            {
+                returnDocument: 'after',
+            },
+        )
+        .then((doc) => {
+            if(!doc) {
+                return res.status(404).json({
+                    message: 'article not found!'
+                })
+            }
+            res.json(doc);
+        })
+        .catch(err => {
+            if(err) {
+                console.log(err);
+                return res.status(500).json({
+                    message: 'failed to return article!!'
+                })
+            }
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'article not found'
+        })
+    }
+};
+
+export const remove = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        PostModel.findOneAndDelete({
+            _id: postId,
+        })
+        .then( doc => {
+            if(!doc) {
+                return res.status(404).json({
+                    message: 'article not found'
+                })
+            }
+            res.json({
+                success: true,
+            })
+        })
+        .catch(err => {
+            if(err) {
+                console.log(err);
+                return res.status(500).json({
+                    message: 'failed to delete article'
+                })
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'articles not found'
+        })
+    }
+};
+
 export const create = async (req, res) => {
     try {
         const doc = new PostModel({
@@ -19,4 +105,29 @@ export const create = async (req, res) => {
             message: 'failed to create article'
         })
     }
-}
+};
+
+export const update = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        await PostModel.updateOne({
+            _id: postId,
+        }, {
+            title: req.body.title,
+            text: req.body.text,
+            imageUrl: req.body.imageUrl,
+            user: req.body.userId,
+            tags: req.body.tags,
+        });
+
+        res.json({
+            success: true,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'failed to update article'
+        })
+    }
+};
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import  express  from "express";
 import mongoose from "mongoose";
+import multer from "multer";
 import { registerValidation, loginValidation, postCreateValidation } from "./validations.js";
 import checkAuth from "./utils/checkAuth.js";
 import * as UserController from "./controllers/UserController.js";
@@ -13,17 +14,22 @@ mongoose
 ;
 
 const app = express();
+
+const storage = multer.diskStorage({
+    
+});
+
 app.use(express.json());
 
 app.post('/auth/login', loginValidation, UserController.login);
 app.post('/auth/register', registerValidation, UserController.registr);
 app.get('/auth/me', checkAuth, UserController.getMe);
 
-// app.get('/posts', checkAuth, PostController.getAll);
-// app.get('/posts/:id', checkAuth, PostController.getOne);
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, PostController.create);
-// app.delete('/posts', checkAuth, PostController.remove);
-// app.patch('/posts', checkAuth, PostController.update);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, PostController.update);
 
 
 app.listen(4444, (err) => {
