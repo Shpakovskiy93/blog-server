5f24a20d89c7a49f6a225cb744613bbf0296d4ee
Shpakovskiy93 <mikhail.shpakovskiy.93@gmail.com>
2023-07-11 20:54:10 +0300

    refactoring

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,20 @@
 import PostModel from '../models/Post.js';
 
+export const getLastTags = async (req, res) => {
+    try {
+        const posts = await PostModel.find().limit(5).exec();
+
+        const tags = posts.map(obj => obj.tags).flat().slice(0, 5);
+
+        res.json(tags);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'articles not found'
+        })
+    }
+};
+
 export const getAll = async (req, res) => {
     try {
         const posts = await PostModel.find().populate('user').exec();
@@ -27,7 +42,7 @@ export const getOne = async (req, res) => {
             {
                 returnDocument: 'after',
             },
-        )
+        ).populate('user')
         .then((doc) => {
             if(!doc) {
                 return res.status(404).json({
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import  express  from "express";
 import mongoose from "mongoose";
 import multer from "multer";
+import cors from "cors";
 import { registerValidation, loginValidation, postCreateValidation } from "./validations.js";
 import {checkAuth, handleValidationErrors} from "./utils/index.js";
 import  {UserController, PostController} from "./controllers/index.js";
@@ -26,6 +27,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
@@ -38,7 +40,10 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
     })
 });
 
+app.get('/tags', PostController.getLastTags);
+
 app.get('/posts', PostController.getAll);
+app.get('/posts/tags', PostController.getLastTags);
 app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
 app.delete('/posts/:id', checkAuth, PostController.remove);
